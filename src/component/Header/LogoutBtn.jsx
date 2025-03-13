1087ededa18249869093b51b71c05bab87431122
Ayush ghosh <ghoshayush910@gmail.com>
2025-03-13 23:47:46 +0530

    component complete

---
+++ src/component/Header/LogoutBtn.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import {useDispatch} from 'react-redux'
+import authservice from '../../Appwrite/Config'
+import {logout} from '../../Store/autSlice'
+
+function LogoutBtn() {
+    const dispatch = useDispatch()
+    const logoutHandler = () => {
+        authservice.logout().then(() => {
+            dispatch(logout())
+        })
+    }
+  return (
+    <button className='inline-bock px-6 py-2 duration-200 hover:bg-blue-100 rounded-full'
+    onClick={logoutHandler}>
+        LogOut
+    </button>
+  )
+}
+
+export default LogoutBtn
